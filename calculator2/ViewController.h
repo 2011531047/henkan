28f6f7794a9764d0530a553880e749605fcb4a27
Nogami Hazuki <haapinn@gmail.com>
2014-01-23 14:33:57 +0900

    数字を表示させた。

--- calculator2/ViewController.h
+++ calculator2/ViewController.h
@@ -9,5 +9,28 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+    {
+        int nownumber,beforenumber;
+    }
+
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+
+- (IBAction)AC:(id)sender;
+
+- (IBAction)henkan1:(id)sender;
+- (IBAction)henkan2:(id)sender;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *result1;
+@property (weak, nonatomic) IBOutlet UILabel *result2;
 
 @end
--- calculator2/ViewController.m
+++ calculator2/ViewController.m
@@ -26,4 +26,86 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+- (IBAction)zero:(id)sender {
+    nownumber = nownumber *10;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+
+}
+
+- (IBAction)one:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +1;
+    _result1.text = [NSString
+                     stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)two:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +2;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)three:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +3;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)four:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +4;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];}
+
+- (IBAction)five:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +5;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)six:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +6;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)seven:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +7;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];}
+
+- (IBAction)eight:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +8;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)nine:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +9;
+    _result1.text = [NSString
+                    stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)AC:(id)sender {
+    nownumber = 0;
+    beforenumber = 0;
+    _result1.text = [NSString stringWithFormat:@"%d", nownumber];
+}
+
+- (IBAction)henkan1:(id)sender {
+}
+
+- (IBAction)henkan2:(id)sender {
+}
+
 @end
